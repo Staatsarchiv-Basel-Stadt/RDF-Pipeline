4ef5ea58acd5ca2b5172c63bdb271a8b904be5fe
Adrian Gschwend <ktk@netlabs.org>
2019-05-22 22:25:03 +0200

    Change to COPY, add metadata

--- shell/oracle-materialize.js
+++ shell/oracle-materialize.js
@@ -1,6 +1,14 @@
 
 const fetch = require('node-fetch')
 
+const metadata = `
+INSERT  { GRAPH <http://data.alod.ch/graph/bs> {
+  <http://data.staatsarchiv-bs.ch/dataset/Archivkatalog> <http://purl.org/dc/terms/modified> ?modified .
+}}  WHERE {
+  BIND( NOW() as ?modified)
+}
+`
+
 function checkStatus (res) {
   if (res.ok) { // res.status >= 200 && res.status < 300
     console.log('Request successfully executed')
@@ -10,8 +18,8 @@ function checkStatus (res) {
   }
 }
 
-fetch('http://admin:admin@pdstavs13:5820/scope/update?query=CLEAR default')
+fetch('http://admin:admin@pdstavs13:5820/scope/update?query=COPY <virtual://scope-virtual> TO <http://data.alod.ch/graph/bs>')
   .then(checkStatus)
-  .then(() => fetch('http://admin:admin@pdstavs13:5820/scope/update?query=ADD <virtual://scope-virtual> TO <http://example.org/scope>'))
+  .then(() => fetch(`http://admin:admin@pdstavs13:5820/scope/update?query=${metadata}`))
   .then(checkStatus)
   .catch((err) => console.error(err))
