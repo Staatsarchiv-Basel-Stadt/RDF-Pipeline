04657798d29c5b01d21e8d3b9c5998a45b76e83d
Adrian Gschwend <ktk@netlabs.org>
2019-07-16 14:49:18 +0200

    Post to LINDAS as cron

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,13 @@ module.exports = {
     cron_restart: '0 5 * * MON',
     watch: false,
     autorestart: false
+  },
+  {
+    name: 'cron-post-lindas',
+    script: './node_modules/.bin/barnard59 --format text/turtle pipelines/staatsarchiv.ttl',
+    cron_restart: '0 6 * * MON',
+    watch: false,
+    autorestart: false
   }
   ]
 }
