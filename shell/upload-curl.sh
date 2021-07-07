db4e6af1ae54a167aef22f9d6a154295677f5d41
Adrian Gschwend <ktk@netlabs.org>
2021-07-07 17:38:29 +0200

    Split files & post in chunks

--- shell/upload-curl.sh
+++ shell/upload-curl.sh
@@ -1,6 +1,37 @@
- curl -X PUT \
-     -n \
-     -H Content-Type:application/n-triples \
-     -T  scope.nt \
-     -G $SINK_ENDPOINT_URL \
-     --data-urlencode graph=https://ld.staatsarchiv.bs.ch/graph/ais-metadata
+#!/bin/bash
+
+FIRST=true
+
+mkdir input
+cd input
+split -l 100000 --additional-suffix=.nt ../scope.nt
+cd ..
+
+for file in ./input/*
+do
+  filename=$(basename "$file")
+  echo "Processing "$filename
+  if [ $FIRST == true ]
+  then
+    echo "PUTing to "$SINK_ENDPOINT_URL
+    FIRST=false
+    curl -X PUT \
+      -n \
+      -H Content-Type:application/n-triples \
+      -T  $filename \
+      -G $SINK_ENDPOINT_URL \
+      --data-urlencode graph=https://ld.staatsarchiv.bs.ch/graph/ais-metadata
+  else
+    echo "POSTing to "$SINK_ENDPOINT_URL
+    curl -X POST \
+      -n \
+      -H Content-Type:application/n-triples \
+      -T  $filename \
+      -G $SINK_ENDPOINT_URL \
+      --data-urlencode graph=https://ld.staatsarchiv.bs.ch/graph/ais-metadata
+  fi 
+
+done;
+
+rm -rf input
+
